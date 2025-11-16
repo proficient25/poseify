521dca0f0954a800782f1c5e7fb397faeb8b052a
Pravin Ram <pravinram2017@gmail.com>
2025-11-16 14:34:14 +0530

    Loader Modified

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@
 
     // Spinner
     var spinner = function () {
-        setTimeout(function () {
+        $(window).on('load', function () {
             if ($('#spinner').length > 0) {
                 $('#spinner').removeClass('show');
             }
-        }, 1);
+        });
     };
     spinner();
     
